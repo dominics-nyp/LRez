2e983225e79feff62ce10cc7ea1f4c9d1dfb0c77
Firman <145768k@mymail.nyp.edu.sg>
2016-12-16 10:22:19 +0800

    Test Commit

--- LRezLib/LRezLib/Managers/ReservationsManager.cs
+++ LRezLib/LRezLib/Managers/ReservationsManager.cs
@@ -27,7 +27,7 @@ namespace LRezLib.Managers
 
         public static bool addReservation(Reservation r)
         {
-            string sql = "insert into Reservations";
+            string sql = "insert into Reservations 000";
 
 
 
